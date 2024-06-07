4847409f1c5633c4da43188a658ebddbe865b5f3
BlackPony32 <113903648+BlackPony32@users.noreply.github.com>
2024-06-07 11:56:02 +0300

    docker update

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,3 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . .
 
-CMD uvicorn main:app --port=8000 --host=0.0.0.0
---
+++ cleanup_docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop all running containers
+sudo docker stop $(sudo docker ps -aq)
+
+# Remove all containers
+sudo docker rm $(sudo docker ps -aq)
+
+# Remove all images
+sudo docker rmi $(sudo docker images -q)
+
+echo "Docker cleanup completed."
